093708de06d661f3ac55624db330599310417c29
Nathan22177 <nathanwillfox@gmail.com>
2019-10-21 12:32:25 +0300

    initial commit

---
+++ src/test/java/com/nathan22177/biddingcommon/BiddingCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nathan22177.biddingcommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiddingCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
