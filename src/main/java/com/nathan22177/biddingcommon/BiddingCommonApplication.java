3baba45416df6cc66d33d2a8352c0ff181c2a218
Nathan22177 <nathanwillfox@gmail.com>
2019-10-21 13:11:28 +0300

    installing library to share common entities between server and client

---
+++ src/main/java/com/nathan22177/biddingcommon/BiddingCommonApplication.java
@@ -0,0 +1,13 @@
+package com.nathan22177.biddingcommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiddingCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiddingCommonApplication.class, args);
+	}
+
+}
