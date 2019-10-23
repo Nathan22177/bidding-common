78dc2bd16dfe34a66d29c8ddaced386e8a4c6a34
Nathan22177 <nathanwillfox@gmail.com>
2019-10-23 22:48:48 +0300

    adding DTOs for interface

--- src/main/java/com/nathan22177/game/dto/StateDTO.java
+++ src/main/java/com/nathan22177/game/dto/StateDTO.java
@@ -4,15 +4,23 @@ import java.util.List;
 
 import com.nathan22177.bidder.BidderPlayer;
 import com.nathan22177.collection.BiddingRound;
+import com.nathan22177.game.Conditions;
+import com.nathan22177.game.PlayerVersusBotGame;
 
+import lombok.Getter;
+
+@Getter
 public class StateDTO {
     private int balance;
     private int acquiredAmount;
+    private Conditions conditions;
     private List<BiddingRound> biddingHistory;
 
-    public StateDTO(BidderPlayer player) {
+    public StateDTO(PlayerVersusBotGame game) {
+        BidderPlayer player = game.getBluePlayer();
         this.balance = player.getBalance();
         this.acquiredAmount = player.getBalance();
         this.biddingHistory = player.getBiddingHistory();
+        this.conditions = game.getConditions();
     }
 }
