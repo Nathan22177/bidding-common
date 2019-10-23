380a0a2e39a32803632d92a266204433ac0c1448
Nathan22177 <nathanwillfox@gmail.com>
2019-10-23 22:59:19 +0300

    JPA...

--- src/main/java/com/nathan22177/bidder/BidderPlayer.java
+++ src/main/java/com/nathan22177/bidder/BidderPlayer.java
@@ -2,13 +2,23 @@ package com.nathan22177.bidder;
 
 import javax.persistence.Embeddable;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 
 import org.springframework.util.Assert;
+
+import com.nathan22177.game.Conditions;
 
 @Entity(name = "bidder_players")
 @Embeddable
 public class BidderPlayer extends AbstractBidder {
+    public BidderPlayer(Conditions conditions) {
+        Assert.isTrue(conditions.getQuantity() % 2 == 0 && conditions.getQuantity() > 0, "Quantity must be a positive and even number.");
+        Assert.isTrue(conditions.getCash() > 0, "Cash must be positive number.");
+        setConditions(conditions);
+        setBalance(conditions.getCash());
+    }
 
+    /**
+     * Used by JPA.
+     */
+    public BidderPlayer() {}
 }
